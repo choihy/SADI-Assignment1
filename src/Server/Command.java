badd7110d3463f6589826b3e483f60bebd2a5ac6
choi hong yaip <Choihy1992@gmail.com>
2014-05-14 12:38:49 +0800

    update

--- src/Server/Command.java
+++ src/Server/Command.java
@@ -1,14 +1,17 @@
 package Server;
 
 public class Command {
+	// login
 	public static final int nameTaken = 1001;
 	public static final int connectFail = 1002;
 	public static final int connectSuccess = 1003;
+	// game 
 	public static final int connectCorupted = 1004;
 	public static final int call = 2001;
 	public static final int pass = 2002;
 	public static final int win = 2003;
 	public static final int lose = 2004;
+	// menu
 	public static final int join = 2005;
-	public static final int send = 2006;
+	public static final int observe = 2006;
 }
